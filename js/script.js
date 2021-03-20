45b918fb84609e2cbd4ae6d649d4f963d0fd2975
mixelio <78027735+mixelio@users.noreply.github.com>
2021-03-21 00:26:30 +0200

    second page

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ $(document).ready(function () {
     speed: 500,
   });
 });
+
+$(document).ready(function () {
+  $(".pets_slider").slick({
+    arrows: true,
+    dots: true,
+    slidesToShow: 4,
+    infinite: true,
+    speed: 500,
+    rows: 2,
+    slidesToScroll: 4,
+  });
+});
